0f6b6d3f55c36f2d5bcb5a6a55f70df14fa845cb
Patrick Schoenfeld <patrick.schoenfeld@credativ.de>
2012-06-12 13:13:51 +0200

    Initial import of module template

---
+++ manifests/init.pp
@@ -0,0 +1,48 @@
+class __module__ (
+    $ensure             = params_lookup('ensure'),
+    $ensure_running     = params_lookup('ensure_running'),
+    $ensure_enabled     = params_lookup('ensure_enabled'),
+    $manage_config      = params_lookup('manage_config'),
+    $config_file        = params_lookup('config_file'),
+    $config_source      = params_lookup('config_source'),
+    $config_template    = params_lookup('config_template'),
+    $disabled_hosts     = params_lookup('disabled_hosts'),
+    ) {
+
+    package { '__module__':
+        ensure => $ensure
+    }
+
+    service { '__module__':
+        ensure      => $ensure,
+        enable      => $ensure_enabled,
+        hasrestart  => true,
+        hasstatus   => true,
+        require     => Package['__module__']
+    }
+
+    file { $config_file:
+        mode    => '0644',
+        owner   => 'root',
+        group   => 'root',
+        tag     => '__module___config'
+    }
+
+    # Disable service on this host, if hostname is in disabled_hosts
+    if $::hostname in $disabled_hosts {
+        Service <| title == '__module__' |> {
+            ensure  => 'stopped',
+            enabled => false,
+        }
+    }
+
+    if $config_source {
+        File <| tag == '__module___config' |> {
+            source  => $config_source
+        }
+    } elsif $config_template {
+        File <| tag == '__module___config' |> {
+            template => $config_template
+        }
+    }
+}
---
+++ manifests/params.pp
@@ -0,0 +1,11 @@
+class __module__::params {
+    $ensure         = 'present'
+    $ensure_running = true
+    $ensure_enabled = true
+    $manage_config      = true
+    $config_file        = '/etc/__module__/__module__.conf'
+    $config_template    = undef
+    $config_source      = undef
+    $disabled_hosts     = []
+}
+
