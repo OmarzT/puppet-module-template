598b8ffea9a96ef262934d96c2d703dd2ee388e3
Patrick Schoenfeld <patrick.schoenfeld@credativ.de>
2012-07-18 09:56:25 +0200

    Add docu to module

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,3 +1,39 @@
+# = Class: __module__
+#
+# Module to manage __module__
+#
+# == Requirements:
+#
+# - This module makes use of the example42 functions in the puppi module
+#   (https://github.com/credativ/puppet-example42lib)
+#
+# == Parameters:
+#
+# [* ensure *]
+#   What state to ensure for the package. Accepts the same values
+#   as the parameter of the same name for a package type.
+#   Default: present
+#   
+# [* ensure_running *]
+#   Weither to ensure running __module__ or not.
+#   Default: running
+#
+# [* ensure_enabled *]
+#   Weither to ensure that __module__ is started on boot or not.
+#   Default: true
+#
+# [* config_source *]
+#   Specify a configuration source for the configuration. If this
+#   is specified it is used instead of a template-generated configuration
+#
+# [* config_template *]
+#   Override the default choice for the configuration template
+#
+# [* disabled_hosts *]
+#   A list of hosts whose __module__ will be disabled, if their
+#   hostname matches a name in the list.
+#
+
 class __module__ (
     $ensure             = params_lookup('ensure'),
     $ensure_running     = params_lookup('ensure_running'),
@@ -47,3 +83,4 @@ class __module__ (
         }
     }
 }
+
